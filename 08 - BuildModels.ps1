39b0d788786704cbc707390f73e2e1501c8a253f
fsilva-jr <50589817+fsilva-jr@users.noreply.github.com>
2025-06-25 11:00:33 +0100

    Update 08 - BuildModels.ps1

--- 08 - BuildModels.ps1
+++ 08 - BuildModels.ps1
@@ -2,6 +2,7 @@
 # XKTools - Compile All Models (Simplified)
 # Created by: Francisco Silva
 # Contact: francisco@mtxn.com.br
+# Updated for PS 5.1 & PS 7+ by PowerShell GPT
 # ============================================
 
 # Auto-elevate if not administrator
