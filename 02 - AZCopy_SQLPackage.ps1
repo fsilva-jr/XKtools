db72f293ba9870adba59ad8a24846530e3b9c7e7
fsilva-jr <50589817+fsilva-jr@users.noreply.github.com>
2025-06-25 10:59:17 +0100

    Update 02 - AZCopy_SQLPackage.ps1

--- 02 - AZCopy_SQLPackage.ps1
+++ 02 - AZCopy_SQLPackage.ps1
@@ -2,6 +2,7 @@
 # XKTools - Download AZCopy and SQLPackage
 # Created by: Francisco Silva
 # Contact: francisco@mtxn.com.br
+# Updated for PS 5.1 & PS 7+ by PowerShell GPT
 # ============================================
 
 # Auto-elevate to admin (using pwsh if needed)
